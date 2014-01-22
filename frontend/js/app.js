ac81932aeb466fb92a15e391de44cf209298b02e
Keith Barber <kbarber@yahoo-inc.com>
2014-01-21 23:55:05 -0800

    * Create a model for the B1G schools with lots of real fixture data * Reference the school models in the matchup instead of the strings * New computed property to set the colors of the row based on the selected winner.   This is currently hacked because it seems to be impossible to get the complete   actual school instance from the matchup model.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -13,6 +13,8 @@ Bsc.BscRoute = Ember.Route.extend({
 });
 
 Bsc.BscController = Ember.ArrayController.extend({
+    itemController: 'matchup',
+    
     pointsAllocated: function() {
 	return this.reduce(function(prev, current) {
 	    if (prev == undefined) { prev = 0; }
@@ -22,38 +24,182 @@ Bsc.BscController = Ember.ArrayController.extend({
     }.property('@each.points')
 });
 
+Bsc.MatchupController = Ember.ObjectController.extend({
+    
+    htmlColors: function() {
+	var winner = this.get('winner');
+	if (winner == null) return "";
+
+	// gross hack for now, I really hope this works when we use RESTAdapter...
+	var c = Bsc.School.FIXTURES.filter(function(school) {
+	    return school.id == winner.id;
+	});
+	winner = c[0];
+	
+	return 'color:' + winner.color1 + ';background-color:' + winner.color2;
+    }.property('winner'),
+});
+
+Bsc.School = DS.Model.extend({
+    name: DS.attr('string'),
+    abbrev: DS.attr('string'),
+    mascot: DS.attr('string'),
+    color1: DS.attr('string'),
+    color2: DS.attr('string'),
+
+    htmlColors: function() {
+	return 'color:' + this.get('color1') + ';background-color:' + this.get('color2');
+    }.property('color1', 'color2'),
+});
+
 Bsc.Matchup = DS.Model.extend({
-    awayTeam: DS.attr('string'),
-    homeTeam: DS.attr('string'),
+    awayTeam: DS.belongsTo('school'),
+    homeTeam: DS.belongsTo('school'),
     line: DS.attr('number'),
     points: DS.attr('number', { defaultValue: 0 }),
+    winner: DS.belongsTo('school'),
 
     teams: function() {
+	var test = this.get('awayTeam').get('name');
 	return [this.get('awayTeam'), this.get('homeTeam')];
     }.property('awayTeam', 'homeTeam'),
 
     homeIsFavored: function() {
 	return this.get('line') < 0;
     }.property('line'),
+/*
+    htmlColors: function() {
+	var winner = this.get('winner');
+	if (winner == null) return "";
+
+	var c = Bsc.School.FIXTURES.filter(function(school) {
+	    return school.id == winner.id;
+	});
+	winner = c[0];
+	
+	return 'color:' + winner.color1 + ';background-color:' + winner.color2;
+    }.property('winner'),
+    */
 });
 
 Bsc.Matchup.FIXTURES = [
     {
 	id: 1,
-	awayTeam: 'MSU',
-	homeTeam: 'NW',
-	line: 7.5
+	awayTeam: 'Michigan State University',
+	homeTeam: 'Northwestern University',
+	line: 7.5,
+	winner: 'Michigan State University',
     },
     {
 	id: 2,
-	awayTeam: 'Illinois',
-	homeTeam: 'Purdue',
-	line: 6.5
+	awayTeam: 'University of Illinois',
+	homeTeam: 'Purdue University',
+	line: 6.5,
     },
     {
 	id: 3,
-	awayTeam: 'Michigan',
-	homeTeam: 'Iowa',
-	line: -6.5
+	awayTeam: 'University of Michigan',
+	homeTeam: 'University of Iowa',
+	line: -6.5,
+	winner: 'University of Michigan'
+    },
+];
+
+Bsc.School.FIXTURES = [
+    {
+	id: 'University of Illinois',
+	name: 'Illinois',
+	abbrev: 'UI',
+	mascot: 'Illini',
+	color1: '#003C7D',
+	color2: '#F47F24',
+    },
+    {
+	id: 'Indiana University',
+	name: 'Indiana',
+	abbrev: 'IU',
+	mascot: 'Hoosiers',
+	color1: '#7D110C',
+	color2: '#E1D8B7',
+    },
+    {
+	id: 'University of Iowa',
+	name: 'Iowa',
+	abbrev: 'Iowa',
+	mascot: 'Hawkeyes',
+	color1: '#FFE100',
+	color2: '#000000'
+    },
+    {
+	id: 'University of Michigan',
+	name: 'Michigan',
+	abbrev: 'UMich',
+	mascot: 'Wolverines',
+	color1: '#ffcb05',
+	color2: '#00274c',
+    },
+    {
+	id: 'Michigan State University',
+	name: 'Michigan State',
+	abbrev: 'MSU',
+	mascot: 'Spartans',
+	color2: '#18453B',
+	color1: '#FFFFFF',
+    },
+    {
+	id: 'University of Minnesota',
+	name: 'Minnesota',
+	abbrev: 'UMinn',
+	mascot: 'Golden Gopher',
+	color1: '#8C1919',
+	color2: '#CCFF33'
+    },
+    {
+	id: 'University of Nebraska',
+	name: 'Nebraska',
+	abbrev: 'NEB',
+	mascot: 'Cornhuskers',
+	color1: '#CC0000',
+	color2: '#F5F3EB',
+    },
+    {
+	id: 'Northwestern University',
+	name: 'Northwestern',
+	abbrev: 'NW',
+	mascot: 'Wildcats',
+	color2: '#520063',
+	color1: '#FFFFFF',
+    },
+    {
+	id: 'Ohio State University',
+	name: 'Ohio State',
+	abbrev: 'OSU',
+	mascot: 'Buckeyes',
+	color1: '#BB0000',
+	color2: '#666666',
+    },
+    {
+	id: 'Pennsylvania State University',
+	name: 'Penn State',
+	abbrev: 'PSU',
+	mascot: 'Nittany Lions',
+	color1: '#003893',
+	color2: '#FFFFFF',
+    },
+    {
+	id: 'Purdue University',
+	name: 'Purdue',
+	abbrev: 'Purdue',
+	mascot: 'Boilermakers',
+	color1: '#BF910C',
+	color2: '#000000',
+    },
+    {
+	id: 'University of Wisconsin',
+	name: 'Wisconsin',
+	abbrev: 'WI',
+	mascot: 'Badgers',
+	color1: '#C41E3A',
+	color2: '#FFFFFF',
     },
 ];
