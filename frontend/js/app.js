6374af579b99db52b38e8f6528119fa5fdf609f0
Keith Barber <kbarber@yahoo-inc.com>
2014-06-15 15:46:21 -0700

    Generate the rows/columns dynamically (with hardcoded JS objects for now)

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,20 +1,37 @@
 window.App = Ember.Application.create();
 
+
 App.BscController = Em.Controller.extend({
     things: [
-    Em.Object.create({ foo: 'a', bar: 'b' }), 
-    Em.Object.create({ foo: 1, bar: 2 })
-  ],
-  fieldNames: ['foo', 'bar'],
+	Em.Object.create({ name: 'Keith', game1: 'a', game2: 'b', game3: 'c' }), 
+	Em.Object.create({ name: 'Aaron', game1: 1, game2: 2, game3: 3 }),
+	Em.Object.create({ name: 'Frank', game1: 'a1', game2: 'b1', game3: 'c1' }),
+    ],
+
+    matchups: [
+	Em.Object.create({ id: 'game1', name: 'MSU vs. NU' }),
+	Em.Object.create({ id: 'game2', name: 'UI vs. PU'}),
+	Em.Object.create({ id: 'game3', name: 'Mich vs. Iowa'}),
+    ],
+
+  printNames: function() {
+      return this.matchups.map(function(t) {
+	  return t.name;
+      });
+  }.property(),
+
+    fieldNames: function() {
+	return ['game1', 'game2', 'game3'];
+    }.property(),
 
   thingsWithFields: function() {
-    var fieldNames = this.get('fieldNames');
+      var fieldNames = this.get('fieldNames');
 
     var thingWithFieldsProxy = Em.ObjectProxy.extend({
       fields: function() {
         var thing = this;
 
-        return fieldNames.map(function(fn) {
+        return names.map(function(fn) {
           // FIX: this returns a raw value which is not bindable in a template
           return thing.get(fn);
         });
