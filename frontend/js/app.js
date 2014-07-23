d1903893c1023c081ad6f73ee8f3a1e38ae67cbb
Keith Barber <kbarber@yahoo-inc.com>
2014-07-22 23:26:02 -0700

    Major improvements to the tabular view

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,36 @@
 window.App = Ember.Application.create();
 
+App.ApplicationAdapter = DS.RESTAdapter.extend();
+
+App.TestObject = Em.Object.extend({
+    games: null,
+
+    total: function() {
+	var sum = '';
+	this.games.forEach(function(elem) {
+	    sum += elem.score;
+	});
+	return sum;
+    }.property('games.@each.score'),
+});
 
 App.BscController = Em.Controller.extend({
     things: [
-	Em.Object.create({ name: 'Keith', game1: 'a', game2: 'b', game3: 'c' }), 
-	Em.Object.create({ name: 'Aaron', game1: 1, game2: 2, game3: 3 }),
-	Em.Object.create({ name: 'Frank', game1: 'a1', game2: 'b1', game3: 'c1' }),
+	App.TestObject.create({ name: 'Keith',
+				games: [{ name: 'game1', score: 'a', winner: 'MSU' },
+					{ name: 'game2', score: 'b', winner: 'PU' },
+					{ name: 'game3', score: 'c', winner: 'Mich' }]
+			      }), 
+	App.TestObject.create({ name: 'Aaron',
+				games: [{ name: 'game1', score: 1, winner: 'MSU' },
+					{ name: 'game2', score: 2, winner: 'UI' },
+					{ name: 'game3', score: 3, winner: 'Iowa' }]
+			      }),
+	App.TestObject.create({ name: 'Frank',
+				games: [{ name: 'game1', score: 'a1', winner: 'NU' },
+					{ name: 'game2', score: 'b2', winner: 'PU' },
+					{ name: 'game3', score: 'c3', winner: 'Iowa' }]
+			      }),
     ],
 
     matchups: [
@@ -24,6 +49,10 @@ App.BscController = Em.Controller.extend({
 	return ['game1', 'game2', 'game3'];
     }.property(),
 
+    schools: function() {
+	return [ 'MSU', 'Mich', 'PU', 'UI', 'Iowa', 'NU' ];
+    }.property(),
+
     thingsWithFields: function() {
 	var fieldNames = this.get('fieldNames');
 
@@ -31,7 +60,7 @@ App.BscController = Em.Controller.extend({
 	    fields: function() {
 		var thing = this;
 
-		return names.map(function(fn) {
+		return fieldNames.map(function(fn) {
 		    // FIX: this returns a raw value which is not bindable in a template
 		    return thing.get(fn);
 		});
@@ -49,11 +78,23 @@ App.DynamicInputView = Em.View.extend({
     template:function(context,data){
         var controller = data.data.keywords.controller;
         var fieldNames = controller.get("fieldNames");
-        
+
         var source="";
         fieldNames.forEach(function(fieldName){
-            source+='<td>{{input type="text" valueBinding="content.'+fieldName+'"}}</td>';
+	    var idx = 0;
+	    for (i = 0; i < context.content.games.length; i++) {
+		if (context.content.games[i].name == fieldName) {
+		    idx = i;
+		    break;
+		}
+	    }
+
+            source+='<td>{{view Ember.Select content=controller.schools value=content.games.' + idx + '.winner}}&nbsp;{{input type="text" valueBinding="content.games.'+idx+'.score"}}</td>';
+	    i++;
         });
+
+	source += '<td>{{content.total}}</td>';
+	source += '<td>{{content.games.0.winner}}</td>';
         var template = Em.Handlebars.compile(source);
         return template.call(this,context,data);
     }
