98b3f05b3f402eb924fe49981ff2eea64a779412
Keith Barber <kbarber@yahoo-inc.com>
2014-08-16 12:45:59 -0700

    Implement create/edit for matchups

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -2,7 +2,7 @@ App = Ember.Application.create();
 
 DS.RESTAdapter.reopen({
     host: 'http://localhost:8080',
-    namespace: 'api',
+    namespace: 'api'
 });
 
 App.School = DS.Model.extend({
@@ -11,22 +11,23 @@ App.School = DS.Model.extend({
     abbreviation: DS.attr('string'),
     mascot: DS.attr('string'),
     primaryColor: DS.attr('string'),
-    secondaryColor: DS.attr('string'),
+    secondaryColor: DS.attr('string')
 });
 
 App.Matchup = DS.Model.extend({
     homeTeam: DS.belongsTo('school'),
     awayTeam: DS.belongsTo('school'),
     line: DS.attr('number'),
-    kickoff: DS.attr('date'),
+    kickoff: DS.attr('date')
 });
 
 App.Router.map(function() {
     this.resource('schools', function() {
-	this.route('edit', { path: '/:school_id/edit' });
+	this.route('edit', { path: ':school_id/edit' });
     });
     this.resource('matchups', function() {
 	this.route('new', { path: 'new' });
+	this.route('edit', { path: ':matchup_id/edit' });
     });
 });
 
@@ -36,7 +37,7 @@ App.SchoolsEditController = Ember.ObjectController.extend({
 	var dirty = this.get('model').serialize();
 	var store = this.get('store');
 	this.get('model').save();
-    },
+    }
 });
 
 App.SchoolsRoute = Ember.Route.extend({
@@ -56,34 +57,61 @@ App.MatchupsIndexController = Ember.ObjectController.extend({
 	newMatchup: function() {
 	    this.transitionToRoute('matchups.new');
 	},
-    },
+    }
 });
 
 App.MatchupsNewRoute = Ember.Route.extend({
+    beforeModel: function() {
+	var self = this;
+	return this.store.find('school').then(function(schools) {
+	    self.set('schools', schools);
+	});
+    },
+
     model: function() {
-	return this.store.createRecord('matchup');
+	return {};
     },
 
     setupController: function(controller, model) {
 	controller.set('model', model);
-	this.store.find('school').then(function(schools) {
-	    controller.set('schools', schools);
-	});
+	
+	var schools = this.get('schools');
+	controller.set('schools', schools);
     },
 
     renderTemplate: function() {
-	this.render('matchups.edit', { controller: 'matchupsNew' });
-    },
+	this.render('matchups._form', { controller: 'matchupsNew' });
+    }
 });
 
-App.MatchupsNewController = Ember.ObjectController.extend({
-    content: {},
+App.MatchupsEditRoute = App.MatchupsNewRoute.extend({
+    model: function(params) {
+	return this.store.find('matchup', params.matchup_id);
+    },
+    
+    renderTemplate: function() {
+	this.render('matchups._form', { controller: 'matchupsEdit' });
+    }
+});
 
+App.MatchupsEditController = Ember.ObjectController.extend({
     actions: {
 	save: function() {
 	    this.get('model').save();
 	    this.transitionToRoute('matchups');
-	},
-    },
+	}
+    }
+});
+
+App.MatchupsNewController = App.MatchupsEditController.extend({
+    content: {},
+
+    actions: {
+	save: function() {
+	    var model = this.get('model');
+	    var record = this.store.createRecord('matchup', model);
+	    return record.save();
+	}
+    }
 });
 
